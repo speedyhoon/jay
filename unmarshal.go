02bc9948c9c47c2a754e727733643d1243b01519
Cam <16303791+speedyhoon@users.noreply.github.com>
2023-11-15 00:08:37 +1100

    Initial commit.

---
+++ unmarshal.go
@@ -0,0 +1,15 @@
+package jay
+
+import (
+	"bytes"
+	"fmt"
+)
+
+// MakeUnmarshal ...
+func (s Struct) MakeUnmarshal(b *bytes.Buffer) {
+	b.WriteString(fmt.Sprintf(
+		"func (%s *%s) UnmarshalJ(b []byte) (err error) {\nreturn nil\n}\n",
+		s.ReceiverName(),
+		s.name,
+	))
+}
