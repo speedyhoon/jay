cd2a37c422bcefa717797bb2e345618f63da76c4
Cam <16303791+speedyhoon@users.noreply.github.com>
2023-11-27 13:59:55 +1100

    Rename tag options not to collide with new Go built in min & max functions.

--- generate/tagOptions.go
+++ generate/tagOptions.go
@@ -8,6 +8,11 @@ import (
 )
 
 func (f *field) LoadTagOptions() {
+	const (
+		tagMax = "max"
+		tagMin = "min"
+	)
+
 	f.tag = strings.TrimSpace(f.tag)
 	if f.tag == "" {
 		return
@@ -15,10 +20,10 @@ func (f *field) LoadTagOptions() {
 	for _, c := range strings.Split(f.tag, ",") {
 		d := strings.Split(c, ":")
 		switch g := d[0]; g {
-		case max:
+		case tagMax:
 			loadUint(d[1], &f.tagOptions.Max)
 			f.tagOptions.maxBytes = byteSize(f.tagOptions.Max)
-		case min:
+		case tagMin:
 			loadUint(g, &f.tagOptions.Min)
 		}
 	}
@@ -51,11 +56,6 @@ func byteSize(v uint) uint {
 	}
 	return 8
 }
-
-const (
-	max = "max"
-	min = "min"
-)
 
 func loadUint(g string, f *uint) {
 	uu, err := strconv.ParseUint(g, 10, 64)
