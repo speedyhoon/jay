21d6331f0bb2f898494435b7bfbfde9c5f010863
Cam <16303791+speedyhoon@users.noreply.github.com>
2023-12-13 01:28:12 +1100

    Add unmarshal floats.

--- float.go
+++ float.go
@@ -9,3 +9,11 @@ func WriteFloat32(b []byte, f float32) {
 func WriteFloat64(b []byte, f float64) {
 	WriteUint64(b, math.Float64bits(f))
 }
+
+func ReadFloat32(b []byte) float32 {
+	return math.Float32frombits(ReadUint32(b))
+}
+
+func ReadFloat64(b []byte) float64 {
+	return math.Float64frombits(ReadUint64(b))
+}
