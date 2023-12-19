43e73ea198d0b301bc9ccaa240b5f55bf392b133
Cam <16303791+speedyhoon@users.noreply.github.com>
2023-12-19 15:19:12 +1100

    Combine duplicate test cases for time functions.

--- time_test.go
+++ time_test.go
@@ -7,58 +7,7 @@ import (
 	"time"
 )
 
-func TestTime(t *testing.T) {
-	now := time.Now().UTC()
-	b := make([]byte, 8)
-	jay.WriteTime(b, now)
-	assert.Equal(t, now.Format(time.RFC3339Nano), jay.ReadTime(b).Format(time.RFC3339Nano))
-}
-
-func TestTimeNil(t *testing.T) {
-	zero := time.Time{}
-	b := make([]byte, 8)
-	jay.WriteTime(b, zero)
-	assert.Equal(t, zero.Format(time.RFC3339Nano), jay.ReadTime(b).Format(time.RFC3339Nano))
-}
-
-func TestTimeNanoNow(t *testing.T) {
-
-	now := time.Now().UTC()
-	b := make([]byte, 8)
-	jay.WriteTimeNano(b, now)
-	assert.Equal(t, now.Format(time.RFC3339Nano), jay.ReadTimeNano(b).Format(time.RFC3339Nano))
-}
-
-func TestTimeNano98(t *testing.T) {
-	now := time.Time{}
-	b := make([]byte, 8)
-	jay.WriteTimeNano(b, now)
-	assert.Equal(t, now.Format(time.RFC3339Nano), jay.ReadTimeNano(b).Format(time.RFC3339Nano))
-}
-
-func TestWriteTimeNano3(t *testing.T) {
-	b := make([]byte, 8)
-	tests := map[string]time.Time{
-		"now":   time.Now(),
-		"empty": {},
-		"zero":  time.Time{}.Add(0),
-		"day1":  time.Date(1, time.January, 1, 0, 0, 0, 0, time.UTC),
-		"1677":  time.Date(1677, time.September, 21, 00, 12, 44, 0, time.UTC),
-		"min":   time.Unix(0, MinInt64),
-		"1754":  time.Date(1754, time.August, 30, 22, 43, 41, 0, time.UTC),
-		"1970":  time.Unix(0, 0),
-		"max":   time.Unix(0, MaxInt64),
-		"2262":  time.Date(2262, time.April, 11, 23, 47, 16, 0, time.UTC),
-	}
-	for name, tt := range tests {
-		t.Run(name, func(t *testing.T) {
-			jay.WriteTimeNano(b, tt)
-			assert.Equal(t, tt.UTC().Format(time.RFC3339Nano), jay.ReadTimeNano(b).Format(time.RFC3339Nano))
-		})
-	}
-}
-
-func TestWriteTime555555(t *testing.T) {
+func TestWriteTime(t *testing.T) {
 	b := make([]byte, 8)
 	tests := map[string]time.Time{
 		"now":   time.Now(),
@@ -84,13 +33,31 @@ func TestWriteTime555555(t *testing.T) {
 	}
 }
 
-func TestWriteTimeNano3Zero(t *testing.T) {
+func TestWriteTimeNano(t *testing.T) {
+	b := make([]byte, 8)
+	tests := map[string]time.Time{
+		"now":   time.Now(),
+		"empty": {},
+		"zero":  time.Time{}.Add(0),
+		"day1":  time.Date(1, time.January, 1, 0, 0, 0, 0, time.UTC),
+		"1677":  time.Date(1677, time.September, 21, 00, 12, 44, 0, time.UTC),
+		"min":   time.Unix(0, MinInt64),
+		"1754":  time.Date(1754, time.August, 30, 22, 43, 41, 0, time.UTC),
+		"1970":  time.Unix(0, 0),
+		"max":   time.Unix(0, MaxInt64),
+		"2262":  time.Date(2262, time.April, 11, 23, 47, 16, 0, time.UTC),
+	}
+	for name, tt := range tests {
+		t.Run(name, func(t *testing.T) {
+			jay.WriteTimeNano(b, tt)
+			assert.Equal(t, tt.UTC().Format(time.RFC3339Nano), jay.ReadTimeNano(b).Format(time.RFC3339Nano))
+		})
+	}
+}
+
+func TestWriteTimeNanoZero(t *testing.T) {
 	input := time.Unix(0, MaxInt64-7)
 	b := make([]byte, 8)
-
-	t.Run("time zero",
-		func(t *testing.T) {
-			jay.WriteTimeNano(b, input)
-			assert.Equal(t, time.Time{}.UTC().Format(time.RFC3339Nano), jay.ReadTimeNano(b).Format(time.RFC3339Nano))
-		})
+	jay.WriteTimeNano(b, input)
+	assert.Equal(t, time.Time{}.UTC().Format(time.RFC3339Nano), jay.ReadTimeNano(b).Format(time.RFC3339Nano))
 }
