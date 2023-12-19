29aabee97ba6f640d7120f3404e06582ed5380f5
Cam <16303791+speedyhoon@users.noreply.github.com>
2023-12-19 11:09:38 +1100

    Improve float32 & float64 tests.

--- float_test.go
+++ float_test.go
@@ -1,6 +1,7 @@
-package jay
+package jay_test
 
 import (
+	"github.com/speedyhoon/jay"
 	"github.com/stretchr/testify/assert"
 	"math"
 	"math/rand"
@@ -11,11 +12,10 @@ import (
 func TestFloat32(t *testing.T) {
 	b := make([]byte, 4)
 	for i := 0; i < 1000; i++ {
-		f := rand.Float32() * float32(math.Pow(10, float64(rand.Intn(9)+1)))
-		name := strconv.FormatFloat(float64(f), 'f', -1, 32)
-		t.Run(name, func(t *testing.T) {
-			WriteFloat32(b, f)
-			assert.Equal(t, f, ReadFloat32(b))
+		f := randFloat32()
+		t.Run(fmtFloat32(f), func(t *testing.T) {
+			jay.WriteFloat32(b, f)
+			assert.Equal(t, f, jay.ReadFloat32(b))
 		})
 	}
 }
@@ -23,23 +23,22 @@ func TestFloat32(t *testing.T) {
 func TestFloat32MaxSmallest(t *testing.T) {
 	b := make([]byte, 4)
 	t.Run("max", func(t *testing.T) {
-		WriteFloat32(b, math.MaxFloat32)
-		assert.Equal(t, float32(math.MaxFloat32), ReadFloat32(b))
+		jay.WriteFloat32(b, math.MaxFloat32)
+		assert.Equal(t, float32(math.MaxFloat32), jay.ReadFloat32(b))
 	})
-	t.Run("smallest", func(t *testing.T) {
-		WriteFloat32(b, math.SmallestNonzeroFloat32)
-		assert.Equal(t, float32(math.SmallestNonzeroFloat32), ReadFloat32(b))
+	t.Run("smallestNonzeroFloat32", func(t *testing.T) {
+		jay.WriteFloat32(b, math.SmallestNonzeroFloat32)
+		assert.Equal(t, float32(math.SmallestNonzeroFloat32), jay.ReadFloat32(b))
 	})
 }
 
 func TestFloat64(t *testing.T) {
 	b := make([]byte, 8)
 	for i := 0; i < 1000; i++ {
-		f := rand.Float64() * math.Pow(10, float64(rand.Intn(9)+1))
-		name := strconv.FormatFloat(f, 'f', -1, 64)
-		t.Run(name, func(t *testing.T) {
-			WriteFloat64(b, f)
-			assert.Equal(t, f, ReadFloat64(b))
+		f := randFloat64()
+		t.Run(fmtFloat64(f), func(t *testing.T) {
+			jay.WriteFloat64(b, f)
+			assert.Equal(t, f, jay.ReadFloat64(b))
 		})
 	}
 }
@@ -47,11 +46,27 @@ func TestFloat64(t *testing.T) {
 func TestFloat64MaxSmallest(t *testing.T) {
 	b := make([]byte, 8)
 	t.Run("max", func(t *testing.T) {
-		WriteFloat64(b, math.MaxFloat64)
-		assert.Equal(t, math.MaxFloat64, ReadFloat64(b))
+		jay.WriteFloat64(b, math.MaxFloat64)
+		assert.Equal(t, math.MaxFloat64, jay.ReadFloat64(b))
 	})
-	t.Run("smallest", func(t *testing.T) {
-		WriteFloat64(b, math.SmallestNonzeroFloat64)
-		assert.Equal(t, math.SmallestNonzeroFloat64, ReadFloat64(b))
+	t.Run("smallestNonzeroFloat64", func(t *testing.T) {
+		jay.WriteFloat64(b, math.SmallestNonzeroFloat64)
+		assert.Equal(t, math.SmallestNonzeroFloat64, jay.ReadFloat64(b))
 	})
+}
+
+func randFloat32() float32 {
+	return rand.Float32() * float32(math.Pow(10, float64(rand.Intn(9)+1)))
+}
+
+func randFloat64() float64 {
+	return rand.Float64() * math.Pow(10, float64(rand.Intn(9)+1))
+}
+
+func fmtFloat32(f float32) string {
+	return fmtFloat64(float64(f))
+}
+
+func fmtFloat64(f float64) string {
+	return strconv.FormatFloat(f, 'f', -1, 32)
 }
