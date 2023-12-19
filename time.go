7662ac03ea1c7bdd691476e06f830e1b43a54455
Cam <16303791+speedyhoon@users.noreply.github.com>
2023-12-19 15:21:08 +1100

    Document design decisions about timeZero constant value any why timeZero != 0.

--- time.go
+++ time.go
@@ -12,7 +12,12 @@ func WriteTime(b []byte, t time.Time) {
 	WriteInt64(b, t.Unix())
 }
 
-const timeZero = 1<<63 - 8 // 9223372036854775800
+// timeZero represents the undefined value for time.Time.
+// Zero (0) is not used to prevent errors when time.Unix(0) (1970-01-01) are converted to 0001-01-01.
+// The maximum and minimum values math.MaxInt64 and math.MinInt64 are also not used to prevent collisions
+// with the earliest and last time.Time representable.
+// Seven nanoseconds are deducted from math.MaxInt64 to provide sufficient buffer against the maximum.
+const timeZero = 1<<63 - 8 // (math.MaxInt64-7) 9223372036854775800
 
 func ReadTimeNano(b []byte) (t time.Time) {
 	if i := ReadInt64(b); i != timeZero {
