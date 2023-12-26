c126c25b03950a064a3005c2b036c1eb8e2b28c6
Cam <16303791+speedyhoon@users.noreply.github.com>
2023-12-27 10:46:39 +1100

    Add testdata for structs only with []byte fields.

---
+++ generate/testdata/bytes/pkg.go
@@ -0,0 +1,345 @@
+package main
+
+type One struct {
+	One []byte
+}
+
+type Two struct {
+	One []byte
+	Two []byte
+}
+
+type Three struct {
+	One   []byte
+	Two   []byte
+	Three []byte
+}
+
+type Four struct {
+	One   []byte
+	Two   []byte
+	Three []byte
+	Four  []byte
+}
+
+type Five struct {
+	One   []byte
+	Two   []byte
+	Three []byte
+	Four  []byte
+	Five  []byte
+}
+
+type Six struct {
+	One   []byte
+	Two   []byte
+	Three []byte
+	Four  []byte
+	Five  []byte
+	Six   []byte
+}
+
+type Seven struct {
+	One   []byte
+	Two   []byte
+	Three []byte
+	Four  []byte
+	Five  []byte
+	Six   []byte
+	Seven []byte
+}
+
+type Eight struct {
+	One   []byte
+	Two   []byte
+	Three []byte
+	Four  []byte
+	Five  []byte
+	Six   []byte
+	Seven []byte
+	Eight []byte
+}
+
+type Nine struct {
+	One   []byte
+	Two   []byte
+	Three []byte
+	Four  []byte
+	Five  []byte
+	Six   []byte
+	Seven []byte
+	Eight []byte
+	Nine  []byte
+}
+
+type Ten struct {
+	One   []byte
+	Two   []byte
+	Three []byte
+	Four  []byte
+	Five  []byte
+	Six   []byte
+	Seven []byte
+	Eight []byte
+	Nine  []byte
+	Ten   []byte
+}
+
+type Eleven struct {
+	One    []byte
+	Two    []byte
+	Three  []byte
+	Four   []byte
+	Five   []byte
+	Six    []byte
+	Seven  []byte
+	Eight  []byte
+	Nine   []byte
+	Ten    []byte
+	Eleven []byte
+}
+
+type Twelve struct {
+	One    []byte
+	Two    []byte
+	Three  []byte
+	Four   []byte
+	Five   []byte
+	Six    []byte
+	Seven  []byte
+	Eight  []byte
+	Nine   []byte
+	Ten    []byte
+	Eleven []byte
+	Twelve []byte
+}
+
+type Thirteen struct {
+	One      []byte
+	Two      []byte
+	Three    []byte
+	Four     []byte
+	Five     []byte
+	Six      []byte
+	Seven    []byte
+	Eight    []byte
+	Nine     []byte
+	Ten      []byte
+	Eleven   []byte
+	Twelve   []byte
+	Thirteen []byte
+}
+
+type Fourteen struct {
+	One      []byte
+	Two      []byte
+	Three    []byte
+	Four     []byte
+	Five     []byte
+	Six      []byte
+	Seven    []byte
+	Eight    []byte
+	Nine     []byte
+	Ten      []byte
+	Eleven   []byte
+	Twelve   []byte
+	Thirteen []byte
+	Fourteen []byte
+}
+
+type Fifteen struct {
+	One      []byte
+	Two      []byte
+	Three    []byte
+	Four     []byte
+	Five     []byte
+	Six      []byte
+	Seven    []byte
+	Eight    []byte
+	Nine     []byte
+	Ten      []byte
+	Eleven   []byte
+	Twelve   []byte
+	Thirteen []byte
+	Fourteen []byte
+	Fifteen  []byte
+}
+
+type Sixteen struct {
+	One      []byte
+	Two      []byte
+	Three    []byte
+	Four     []byte
+	Five     []byte
+	Six      []byte
+	Seven    []byte
+	Eight    []byte
+	Nine     []byte
+	Ten      []byte
+	Eleven   []byte
+	Twelve   []byte
+	Thirteen []byte
+	Fourteen []byte
+	Fifteen  []byte
+	Sixteen  []byte
+}
+
+type Seventeen struct {
+	One       []byte
+	Two       []byte
+	Three     []byte
+	Four      []byte
+	Five      []byte
+	Six       []byte
+	Seven     []byte
+	Eight     []byte
+	Nine      []byte
+	Ten       []byte
+	Eleven    []byte
+	Twelve    []byte
+	Thirteen  []byte
+	Fourteen  []byte
+	Fifteen   []byte
+	Sixteen   []byte
+	Seventeen []byte
+}
+type Eighteen struct {
+	One       []byte
+	Two       []byte
+	Three     []byte
+	Four      []byte
+	Five      []byte
+	Six       []byte
+	Seven     []byte
+	Eight     []byte
+	Nine      []byte
+	Ten       []byte
+	Eleven    []byte
+	Twelve    []byte
+	Thirteen  []byte
+	Fourteen  []byte
+	Fifteen   []byte
+	Sixteen   []byte
+	Seventeen []byte
+	Eighteen  []byte
+}
+
+type Nineteen struct {
+	One       []byte
+	Two       []byte
+	Three     []byte
+	Four      []byte
+	Five      []byte
+	Six       []byte
+	Seven     []byte
+	Eight     []byte
+	Nine      []byte
+	Ten       []byte
+	Eleven    []byte
+	Twelve    []byte
+	Thirteen  []byte
+	Fourteen  []byte
+	Fifteen   []byte
+	Sixteen   []byte
+	Seventeen []byte
+	Eighteen  []byte
+	Nineteen  []byte
+}
+
+type Twenty struct {
+	One       []byte
+	Two       []byte
+	Three     []byte
+	Four      []byte
+	Five      []byte
+	Six       []byte
+	Seven     []byte
+	Eight     []byte
+	Nine      []byte
+	Ten       []byte
+	Eleven    []byte
+	Twelve    []byte
+	Thirteen  []byte
+	Fourteen  []byte
+	Fifteen   []byte
+	Sixteen   []byte
+	Seventeen []byte
+	Eighteen  []byte
+	Nineteen  []byte
+	Twenty    []byte
+}
+
+type TwentyOne struct {
+	One       []byte
+	Two       []byte
+	Three     []byte
+	Four      []byte
+	Five      []byte
+	Six       []byte
+	Seven     []byte
+	Eight     []byte
+	Nine      []byte
+	Ten       []byte
+	Eleven    []byte
+	Twelve    []byte
+	Thirteen  []byte
+	Fourteen  []byte
+	Fifteen   []byte
+	Sixteen   []byte
+	Seventeen []byte
+	Eighteen  []byte
+	Nineteen  []byte
+	Twenty    []byte
+	TwentyOne []byte
+}
+
+type TwentyTwo struct {
+	One       []byte
+	Two       []byte
+	Three     []byte
+	Four      []byte
+	Five      []byte
+	Six       []byte
+	Seven     []byte
+	Eight     []byte
+	Nine      []byte
+	Ten       []byte
+	Eleven    []byte
+	Twelve    []byte
+	Thirteen  []byte
+	Fourteen  []byte
+	Fifteen   []byte
+	Sixteen   []byte
+	Seventeen []byte
+	Eighteen  []byte
+	Nineteen  []byte
+	Twenty    []byte
+	TwentyOne []byte
+	TwentyTwo []byte
+}
+
+type TwentyThree struct {
+	One         []byte
+	Two         []byte
+	Three       []byte
+	Four        []byte
+	Five        []byte
+	Six         []byte
+	Seven       []byte
+	Eight       []byte
+	Nine        []byte
+	Ten         []byte
+	Eleven      []byte
+	Twelve      []byte
+	Thirteen    []byte
+	Fourteen    []byte
+	Fifteen     []byte
+	Sixteen     []byte
+	Seventeen   []byte
+	Eighteen    []byte
+	Nineteen    []byte
+	Twenty      []byte
+	TwentyOne   []byte
+	TwentyTwo   []byte
+	TwentyThree []byte
+}
