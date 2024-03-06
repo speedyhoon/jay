dad3b4f152824e2050da95c2977d23030462df69
Cam <16303791+speedyhoon@users.noreply.github.com>
2024-03-06 18:13:07 +1100

    Add tests for tag option nano.

---
+++ generate/testdata/timeTimeNano/pkg.go
@@ -0,0 +1,348 @@
+package main
+
+import "time"
+
+type One struct {
+	One time.Time `j:",nano"`
+}
+
+type Two struct {
+	One time.Time `j:",nano"`
+	Two time.Time `j:",nano"`
+}
+
+type Three struct {
+	One   time.Time `j:",nano"`
+	Two   time.Time `j:",nano"`
+	Three time.Time `j:",nano"`
+}
+
+type Four struct {
+	One   time.Time `j:",nano"`
+	Two   time.Time `j:",nano"`
+	Three time.Time `j:",nano"`
+	Four  time.Time `j:",nano"`
+}
+
+type Five struct {
+	One   time.Time `j:",nano"`
+	Two   time.Time `j:",nano"`
+	Three time.Time `j:",nano"`
+	Four  time.Time `j:",nano"`
+	Five  time.Time `j:",nano"`
+}
+
+type Six struct {
+	One   time.Time `j:",nano"`
+	Two   time.Time `j:",nano"`
+	Three time.Time `j:",nano"`
+	Four  time.Time `j:",nano"`
+	Five  time.Time `j:",nano"`
+	Six   time.Time `j:",nano"`
+}
+
+type Seven struct {
+	One   time.Time `j:",nano"`
+	Two   time.Time `j:",nano"`
+	Three time.Time `j:",nano"`
+	Four  time.Time `j:",nano"`
+	Five  time.Time `j:",nano"`
+	Six   time.Time `j:",nano"`
+	Seven time.Time `j:",nano"`
+}
+
+type Eight struct {
+	One   time.Time `j:",nano"`
+	Two   time.Time `j:",nano"`
+	Three time.Time `j:",nano"`
+	Four  time.Time `j:",nano"`
+	Five  time.Time `j:",nano"`
+	Six   time.Time `j:",nano"`
+	Seven time.Time `j:",nano"`
+	Eight time.Time `j:",nano"`
+}
+
+type Nine struct {
+	One   time.Time `j:",nano"`
+	Two   time.Time `j:",nano"`
+	Three time.Time `j:",nano"`
+	Four  time.Time `j:",nano"`
+	Five  time.Time `j:",nano"`
+	Six   time.Time `j:",nano"`
+	Seven time.Time `j:",nano"`
+	Eight time.Time `j:",nano"`
+	Nine  time.Time `j:",nano"`
+}
+
+type Ten struct {
+	One   time.Time `j:",nano"`
+	Two   time.Time `j:",nano"`
+	Three time.Time `j:",nano"`
+	Four  time.Time `j:",nano"`
+	Five  time.Time `j:",nano"`
+	Six   time.Time `j:",nano"`
+	Seven time.Time `j:",nano"`
+	Eight time.Time `j:",nano"`
+	Nine  time.Time `j:",nano"`
+	Ten   time.Time `j:",nano"`
+}
+
+type Eleven struct {
+	One    time.Time `j:",nano"`
+	Two    time.Time `j:",nano"`
+	Three  time.Time `j:",nano"`
+	Four   time.Time `j:",nano"`
+	Five   time.Time `j:",nano"`
+	Six    time.Time `j:",nano"`
+	Seven  time.Time `j:",nano"`
+	Eight  time.Time `j:",nano"`
+	Nine   time.Time `j:",nano"`
+	Ten    time.Time `j:",nano"`
+	Eleven time.Time `j:",nano"`
+}
+
+type Twelve struct {
+	One    time.Time `j:",nano"`
+	Two    time.Time `j:",nano"`
+	Three  time.Time `j:",nano"`
+	Four   time.Time `j:",nano"`
+	Five   time.Time `j:",nano"`
+	Six    time.Time `j:",nano"`
+	Seven  time.Time `j:",nano"`
+	Eight  time.Time `j:",nano"`
+	Nine   time.Time `j:",nano"`
+	Ten    time.Time `j:",nano"`
+	Eleven time.Time `j:",nano"`
+	Twelve time.Time `j:",nano"`
+}
+
+type Thirteen struct {
+	One      time.Time `j:",nano"`
+	Two      time.Time `j:",nano"`
+	Three    time.Time `j:",nano"`
+	Four     time.Time `j:",nano"`
+	Five     time.Time `j:",nano"`
+	Six      time.Time `j:",nano"`
+	Seven    time.Time `j:",nano"`
+	Eight    time.Time `j:",nano"`
+	Nine     time.Time `j:",nano"`
+	Ten      time.Time `j:",nano"`
+	Eleven   time.Time `j:",nano"`
+	Twelve   time.Time `j:",nano"`
+	Thirteen time.Time `j:",nano"`
+}
+
+type Fourteen struct {
+	One      time.Time `j:",nano"`
+	Two      time.Time `j:",nano"`
+	Three    time.Time `j:",nano"`
+	Four     time.Time `j:",nano"`
+	Five     time.Time `j:",nano"`
+	Six      time.Time `j:",nano"`
+	Seven    time.Time `j:",nano"`
+	Eight    time.Time `j:",nano"`
+	Nine     time.Time `j:",nano"`
+	Ten      time.Time `j:",nano"`
+	Eleven   time.Time `j:",nano"`
+	Twelve   time.Time `j:",nano"`
+	Thirteen time.Time `j:",nano"`
+	Fourteen time.Time `j:",nano"`
+}
+
+type Fifteen struct {
+	One      time.Time `j:",nano"`
+	Two      time.Time `j:",nano"`
+	Three    time.Time `j:",nano"`
+	Four     time.Time `j:",nano"`
+	Five     time.Time `j:",nano"`
+	Six      time.Time `j:",nano"`
+	Seven    time.Time `j:",nano"`
+	Eight    time.Time `j:",nano"`
+	Nine     time.Time `j:",nano"`
+	Ten      time.Time `j:",nano"`
+	Eleven   time.Time `j:",nano"`
+	Twelve   time.Time `j:",nano"`
+	Thirteen time.Time `j:",nano"`
+	Fourteen time.Time `j:",nano"`
+	Fifteen  time.Time `j:",nano"`
+}
+
+type Sixteen struct {
+	One      time.Time `j:",nano"`
+	Two      time.Time `j:",nano"`
+	Three    time.Time `j:",nano"`
+	Four     time.Time `j:",nano"`
+	Five     time.Time `j:",nano"`
+	Six      time.Time `j:",nano"`
+	Seven    time.Time `j:",nano"`
+	Eight    time.Time `j:",nano"`
+	Nine     time.Time `j:",nano"`
+	Ten      time.Time `j:",nano"`
+	Eleven   time.Time `j:",nano"`
+	Twelve   time.Time `j:",nano"`
+	Thirteen time.Time `j:",nano"`
+	Fourteen time.Time `j:",nano"`
+	Fifteen  time.Time `j:",nano"`
+	Sixteen  time.Time `j:",nano"`
+}
+
+type Seventeen struct {
+	One       time.Time `j:",nano"`
+	Two       time.Time `j:",nano"`
+	Three     time.Time `j:",nano"`
+	Four      time.Time `j:",nano"`
+	Five      time.Time `j:",nano"`
+	Six       time.Time `j:",nano"`
+	Seven     time.Time `j:",nano"`
+	Eight     time.Time `j:",nano"`
+	Nine      time.Time `j:",nano"`
+	Ten       time.Time `j:",nano"`
+	Eleven    time.Time `j:",nano"`
+	Twelve    time.Time `j:",nano"`
+	Thirteen  time.Time `j:",nano"`
+	Fourteen  time.Time `j:",nano"`
+	Fifteen   time.Time `j:",nano"`
+	Sixteen   time.Time `j:",nano"`
+	Seventeen time.Time `j:",nano"`
+}
+
+type Eighteen struct {
+	One       time.Time `j:",nano"`
+	Two       time.Time `j:",nano"`
+	Three     time.Time `j:",nano"`
+	Four      time.Time `j:",nano"`
+	Five      time.Time `j:",nano"`
+	Six       time.Time `j:",nano"`
+	Seven     time.Time `j:",nano"`
+	Eight     time.Time `j:",nano"`
+	Nine      time.Time `j:",nano"`
+	Ten       time.Time `j:",nano"`
+	Eleven    time.Time `j:",nano"`
+	Twelve    time.Time `j:",nano"`
+	Thirteen  time.Time `j:",nano"`
+	Fourteen  time.Time `j:",nano"`
+	Fifteen   time.Time `j:",nano"`
+	Sixteen   time.Time `j:",nano"`
+	Seventeen time.Time `j:",nano"`
+	Eighteen  time.Time `j:",nano"`
+}
+
+type Nineteen struct {
+	One       time.Time `j:",nano"`
+	Two       time.Time `j:",nano"`
+	Three     time.Time `j:",nano"`
+	Four      time.Time `j:",nano"`
+	Five      time.Time `j:",nano"`
+	Six       time.Time `j:",nano"`
+	Seven     time.Time `j:",nano"`
+	Eight     time.Time `j:",nano"`
+	Nine      time.Time `j:",nano"`
+	Ten       time.Time `j:",nano"`
+	Eleven    time.Time `j:",nano"`
+	Twelve    time.Time `j:",nano"`
+	Thirteen  time.Time `j:",nano"`
+	Fourteen  time.Time `j:",nano"`
+	Fifteen   time.Time `j:",nano"`
+	Sixteen   time.Time `j:",nano"`
+	Seventeen time.Time `j:",nano"`
+	Eighteen  time.Time `j:",nano"`
+	Nineteen  time.Time `j:",nano"`
+}
+
+type Twenty struct {
+	One       time.Time `j:",nano"`
+	Two       time.Time `j:",nano"`
+	Three     time.Time `j:",nano"`
+	Four      time.Time `j:",nano"`
+	Five      time.Time `j:",nano"`
+	Six       time.Time `j:",nano"`
+	Seven     time.Time `j:",nano"`
+	Eight     time.Time `j:",nano"`
+	Nine      time.Time `j:",nano"`
+	Ten       time.Time `j:",nano"`
+	Eleven    time.Time `j:",nano"`
+	Twelve    time.Time `j:",nano"`
+	Thirteen  time.Time `j:",nano"`
+	Fourteen  time.Time `j:",nano"`
+	Fifteen   time.Time `j:",nano"`
+	Sixteen   time.Time `j:",nano"`
+	Seventeen time.Time `j:",nano"`
+	Eighteen  time.Time `j:",nano"`
+	Nineteen  time.Time `j:",nano"`
+	Twenty    time.Time `j:",nano"`
+}
+
+type TwentyOne struct {
+	One       time.Time `j:",nano"`
+	Two       time.Time `j:",nano"`
+	Three     time.Time `j:",nano"`
+	Four      time.Time `j:",nano"`
+	Five      time.Time `j:",nano"`
+	Six       time.Time `j:",nano"`
+	Seven     time.Time `j:",nano"`
+	Eight     time.Time `j:",nano"`
+	Nine      time.Time `j:",nano"`
+	Ten       time.Time `j:",nano"`
+	Eleven    time.Time `j:",nano"`
+	Twelve    time.Time `j:",nano"`
+	Thirteen  time.Time `j:",nano"`
+	Fourteen  time.Time `j:",nano"`
+	Fifteen   time.Time `j:",nano"`
+	Sixteen   time.Time `j:",nano"`
+	Seventeen time.Time `j:",nano"`
+	Eighteen  time.Time `j:",nano"`
+	Nineteen  time.Time `j:",nano"`
+	Twenty    time.Time `j:",nano"`
+	TwentyOne time.Time `j:",nano"`
+}
+
+type TwentyTwo struct {
+	One       time.Time `j:",nano"`
+	Two       time.Time `j:",nano"`
+	Three     time.Time `j:",nano"`
+	Four      time.Time `j:",nano"`
+	Five      time.Time `j:",nano"`
+	Six       time.Time `j:",nano"`
+	Seven     time.Time `j:",nano"`
+	Eight     time.Time `j:",nano"`
+	Nine      time.Time `j:",nano"`
+	Ten       time.Time `j:",nano"`
+	Eleven    time.Time `j:",nano"`
+	Twelve    time.Time `j:",nano"`
+	Thirteen  time.Time `j:",nano"`
+	Fourteen  time.Time `j:",nano"`
+	Fifteen   time.Time `j:",nano"`
+	Sixteen   time.Time `j:",nano"`
+	Seventeen time.Time `j:",nano"`
+	Eighteen  time.Time `j:",nano"`
+	Nineteen  time.Time `j:",nano"`
+	Twenty    time.Time `j:",nano"`
+	TwentyOne time.Time `j:",nano"`
+	TwentyTwo time.Time `j:",nano"`
+}
+
+type TwentyThree struct {
+	One         time.Time `j:",nano"`
+	Two         time.Time `j:",nano"`
+	Three       time.Time `j:",nano"`
+	Four        time.Time `j:",nano"`
+	Five        time.Time `j:",nano"`
+	Six         time.Time `j:",nano"`
+	Seven       time.Time `j:",nano"`
+	Eight       time.Time `j:",nano"`
+	Nine        time.Time `j:",nano"`
+	Ten         time.Time `j:",nano"`
+	Eleven      time.Time `j:",nano"`
+	Twelve      time.Time `j:",nano"`
+	Thirteen    time.Time `j:",nano"`
+	Fourteen    time.Time `j:",nano"`
+	Fifteen     time.Time `j:",nano"`
+	Sixteen     time.Time `j:",nano"`
+	Seventeen   time.Time `j:",nano"`
+	Eighteen    time.Time `j:",nano"`
+	Nineteen    time.Time `j:",nano"`
+	Twenty      time.Time `j:",nano"`
+	TwentyOne   time.Time `j:",nano"`
+	TwentyTwo   time.Time `j:",nano"`
+	TwentyThree time.Time `j:",nano"`
+}
