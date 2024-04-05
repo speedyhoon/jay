58c75d3dac045905001697fee901f8f332d06117
Cam <16303791+speedyhoon@users.noreply.github.com>
2024-04-05 22:21:18 +1100

    Add comments.

--- generate/slice.go
+++ generate/slice.go
@@ -1,7 +1,9 @@
 package generate
 
 func RemoveDuplicates(s []string) []string {
+	// Loop through the first item through to the second last item.
 	for i := 0; i < len(s)-1; i++ {
+		// Loop from item i+1 through to the last item.
 		for j := i + 1; j < len(s); j++ {
 			if s[i] == s[j] {
 				s = Remove(s, j)
