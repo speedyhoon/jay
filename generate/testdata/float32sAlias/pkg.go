b54eff7310bbd956b9775ad96f33bf3f89259884
Cam <16303791+speedyhoon@users.noreply.github.com>
2024-04-09 22:45:32 +1000

    Add testdata for various []float32 types.

---
+++ generate/testdata/float32sAlias/pkg.go
@@ -0,0 +1,348 @@
+package main
+
+type float = float32
+
+type One struct {
+	One []float
+}
+
+type Two struct {
+	One []float
+	Two []float
+}
+
+type Three struct {
+	One   []float
+	Two   []float
+	Three []float
+}
+
+type Four struct {
+	One   []float
+	Two   []float
+	Three []float
+	Four  []float
+}
+
+type Five struct {
+	One   []float
+	Two   []float
+	Three []float
+	Four  []float
+	Five  []float
+}
+
+type Six struct {
+	One   []float
+	Two   []float
+	Three []float
+	Four  []float
+	Five  []float
+	Six   []float
+}
+
+type Seven struct {
+	One   []float
+	Two   []float
+	Three []float
+	Four  []float
+	Five  []float
+	Six   []float
+	Seven []float
+}
+
+type Eight struct {
+	One   []float
+	Two   []float
+	Three []float
+	Four  []float
+	Five  []float
+	Six   []float
+	Seven []float
+	Eight []float
+}
+
+type Nine struct {
+	One   []float
+	Two   []float
+	Three []float
+	Four  []float
+	Five  []float
+	Six   []float
+	Seven []float
+	Eight []float
+	Nine  []float
+}
+
+type Ten struct {
+	One   []float
+	Two   []float
+	Three []float
+	Four  []float
+	Five  []float
+	Six   []float
+	Seven []float
+	Eight []float
+	Nine  []float
+	Ten   []float
+}
+
+type Eleven struct {
+	One    []float
+	Two    []float
+	Three  []float
+	Four   []float
+	Five   []float
+	Six    []float
+	Seven  []float
+	Eight  []float
+	Nine   []float
+	Ten    []float
+	Eleven []float
+}
+
+type Twelve struct {
+	One    []float
+	Two    []float
+	Three  []float
+	Four   []float
+	Five   []float
+	Six    []float
+	Seven  []float
+	Eight  []float
+	Nine   []float
+	Ten    []float
+	Eleven []float
+	Twelve []float
+}
+
+type Thirteen struct {
+	One      []float
+	Two      []float
+	Three    []float
+	Four     []float
+	Five     []float
+	Six      []float
+	Seven    []float
+	Eight    []float
+	Nine     []float
+	Ten      []float
+	Eleven   []float
+	Twelve   []float
+	Thirteen []float
+}
+
+type Fourteen struct {
+	One      []float
+	Two      []float
+	Three    []float
+	Four     []float
+	Five     []float
+	Six      []float
+	Seven    []float
+	Eight    []float
+	Nine     []float
+	Ten      []float
+	Eleven   []float
+	Twelve   []float
+	Thirteen []float
+	Fourteen []float
+}
+
+type Fifteen struct {
+	One      []float
+	Two      []float
+	Three    []float
+	Four     []float
+	Five     []float
+	Six      []float
+	Seven    []float
+	Eight    []float
+	Nine     []float
+	Ten      []float
+	Eleven   []float
+	Twelve   []float
+	Thirteen []float
+	Fourteen []float
+	Fifteen  []float
+}
+
+type Sixteen struct {
+	One      []float
+	Two      []float
+	Three    []float
+	Four     []float
+	Five     []float
+	Six      []float
+	Seven    []float
+	Eight    []float
+	Nine     []float
+	Ten      []float
+	Eleven   []float
+	Twelve   []float
+	Thirteen []float
+	Fourteen []float
+	Fifteen  []float
+	Sixteen  []float
+}
+
+type Seventeen struct {
+	One       []float
+	Two       []float
+	Three     []float
+	Four      []float
+	Five      []float
+	Six       []float
+	Seven     []float
+	Eight     []float
+	Nine      []float
+	Ten       []float
+	Eleven    []float
+	Twelve    []float
+	Thirteen  []float
+	Fourteen  []float
+	Fifteen   []float
+	Sixteen   []float
+	Seventeen []float
+}
+
+type Eighteen struct {
+	One       []float
+	Two       []float
+	Three     []float
+	Four      []float
+	Five      []float
+	Six       []float
+	Seven     []float
+	Eight     []float
+	Nine      []float
+	Ten       []float
+	Eleven    []float
+	Twelve    []float
+	Thirteen  []float
+	Fourteen  []float
+	Fifteen   []float
+	Sixteen   []float
+	Seventeen []float
+	Eighteen  []float
+}
+
+type Nineteen struct {
+	One       []float
+	Two       []float
+	Three     []float
+	Four      []float
+	Five      []float
+	Six       []float
+	Seven     []float
+	Eight     []float
+	Nine      []float
+	Ten       []float
+	Eleven    []float
+	Twelve    []float
+	Thirteen  []float
+	Fourteen  []float
+	Fifteen   []float
+	Sixteen   []float
+	Seventeen []float
+	Eighteen  []float
+	Nineteen  []float
+}
+
+type Twenty struct {
+	One       []float
+	Two       []float
+	Three     []float
+	Four      []float
+	Five      []float
+	Six       []float
+	Seven     []float
+	Eight     []float
+	Nine      []float
+	Ten       []float
+	Eleven    []float
+	Twelve    []float
+	Thirteen  []float
+	Fourteen  []float
+	Fifteen   []float
+	Sixteen   []float
+	Seventeen []float
+	Eighteen  []float
+	Nineteen  []float
+	Twenty    []float
+}
+
+type TwentyOne struct {
+	One       []float
+	Two       []float
+	Three     []float
+	Four      []float
+	Five      []float
+	Six       []float
+	Seven     []float
+	Eight     []float
+	Nine      []float
+	Ten       []float
+	Eleven    []float
+	Twelve    []float
+	Thirteen  []float
+	Fourteen  []float
+	Fifteen   []float
+	Sixteen   []float
+	Seventeen []float
+	Eighteen  []float
+	Nineteen  []float
+	Twenty    []float
+	TwentyOne []float
+}
+
+type TwentyTwo struct {
+	One       []float
+	Two       []float
+	Three     []float
+	Four      []float
+	Five      []float
+	Six       []float
+	Seven     []float
+	Eight     []float
+	Nine      []float
+	Ten       []float
+	Eleven    []float
+	Twelve    []float
+	Thirteen  []float
+	Fourteen  []float
+	Fifteen   []float
+	Sixteen   []float
+	Seventeen []float
+	Eighteen  []float
+	Nineteen  []float
+	Twenty    []float
+	TwentyOne []float
+	TwentyTwo []float
+}
+
+type TwentyThree struct {
+	One         []float
+	Two         []float
+	Three       []float
+	Four        []float
+	Five        []float
+	Six         []float
+	Seven       []float
+	Eight       []float
+	Nine        []float
+	Ten         []float
+	Eleven      []float
+	Twelve      []float
+	Thirteen    []float
+	Fourteen    []float
+	Fifteen     []float
+	Sixteen     []float
+	Seventeen   []float
+	Eighteen    []float
+	Nineteen    []float
+	Twenty      []float
+	TwentyOne   []float
+	TwentyTwo   []float
+	TwentyThree []float
+}
