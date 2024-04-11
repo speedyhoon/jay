93680964d524126d9548466984abeb322b5e117f
Cam <16303791+speedyhoon@users.noreply.github.com>
2024-04-11 15:37:36 +1000

    Add testdata for []int8 alias types.

---
+++ generate/testdata/int8sAliasedDef/pkg.go
@@ -0,0 +1,348 @@
+package main
+
+type tiny []int8
+
+type One struct {
+	One tiny
+}
+
+type Two struct {
+	One tiny
+	Two tiny
+}
+
+type Three struct {
+	One   tiny
+	Two   tiny
+	Three tiny
+}
+
+type Four struct {
+	One   tiny
+	Two   tiny
+	Three tiny
+	Four  tiny
+}
+
+type Five struct {
+	One   tiny
+	Two   tiny
+	Three tiny
+	Four  tiny
+	Five  tiny
+}
+
+type Six struct {
+	One   tiny
+	Two   tiny
+	Three tiny
+	Four  tiny
+	Five  tiny
+	Six   tiny
+}
+
+type Seven struct {
+	One   tiny
+	Two   tiny
+	Three tiny
+	Four  tiny
+	Five  tiny
+	Six   tiny
+	Seven tiny
+}
+
+type Eight struct {
+	One   tiny
+	Two   tiny
+	Three tiny
+	Four  tiny
+	Five  tiny
+	Six   tiny
+	Seven tiny
+	Eight tiny
+}
+
+type Nine struct {
+	One   tiny
+	Two   tiny
+	Three tiny
+	Four  tiny
+	Five  tiny
+	Six   tiny
+	Seven tiny
+	Eight tiny
+	Nine  tiny
+}
+
+type Ten struct {
+	One   tiny
+	Two   tiny
+	Three tiny
+	Four  tiny
+	Five  tiny
+	Six   tiny
+	Seven tiny
+	Eight tiny
+	Nine  tiny
+	Ten   tiny
+}
+
+type Eleven struct {
+	One    tiny
+	Two    tiny
+	Three  tiny
+	Four   tiny
+	Five   tiny
+	Six    tiny
+	Seven  tiny
+	Eight  tiny
+	Nine   tiny
+	Ten    tiny
+	Eleven tiny
+}
+
+type Twelve struct {
+	One    tiny
+	Two    tiny
+	Three  tiny
+	Four   tiny
+	Five   tiny
+	Six    tiny
+	Seven  tiny
+	Eight  tiny
+	Nine   tiny
+	Ten    tiny
+	Eleven tiny
+	Twelve tiny
+}
+
+type Thirteen struct {
+	One      tiny
+	Two      tiny
+	Three    tiny
+	Four     tiny
+	Five     tiny
+	Six      tiny
+	Seven    tiny
+	Eight    tiny
+	Nine     tiny
+	Ten      tiny
+	Eleven   tiny
+	Twelve   tiny
+	Thirteen tiny
+}
+
+type Fourteen struct {
+	One      tiny
+	Two      tiny
+	Three    tiny
+	Four     tiny
+	Five     tiny
+	Six      tiny
+	Seven    tiny
+	Eight    tiny
+	Nine     tiny
+	Ten      tiny
+	Eleven   tiny
+	Twelve   tiny
+	Thirteen tiny
+	Fourteen tiny
+}
+
+type Fifteen struct {
+	One      tiny
+	Two      tiny
+	Three    tiny
+	Four     tiny
+	Five     tiny
+	Six      tiny
+	Seven    tiny
+	Eight    tiny
+	Nine     tiny
+	Ten      tiny
+	Eleven   tiny
+	Twelve   tiny
+	Thirteen tiny
+	Fourteen tiny
+	Fifteen  tiny
+}
+
+type Sixteen struct {
+	One      tiny
+	Two      tiny
+	Three    tiny
+	Four     tiny
+	Five     tiny
+	Six      tiny
+	Seven    tiny
+	Eight    tiny
+	Nine     tiny
+	Ten      tiny
+	Eleven   tiny
+	Twelve   tiny
+	Thirteen tiny
+	Fourteen tiny
+	Fifteen  tiny
+	Sixteen  tiny
+}
+
+type Seventeen struct {
+	One       tiny
+	Two       tiny
+	Three     tiny
+	Four      tiny
+	Five      tiny
+	Six       tiny
+	Seven     tiny
+	Eight     tiny
+	Nine      tiny
+	Ten       tiny
+	Eleven    tiny
+	Twelve    tiny
+	Thirteen  tiny
+	Fourteen  tiny
+	Fifteen   tiny
+	Sixteen   tiny
+	Seventeen tiny
+}
+
+type Eighteen struct {
+	One       tiny
+	Two       tiny
+	Three     tiny
+	Four      tiny
+	Five      tiny
+	Six       tiny
+	Seven     tiny
+	Eight     tiny
+	Nine      tiny
+	Ten       tiny
+	Eleven    tiny
+	Twelve    tiny
+	Thirteen  tiny
+	Fourteen  tiny
+	Fifteen   tiny
+	Sixteen   tiny
+	Seventeen tiny
+	Eighteen  tiny
+}
+
+type Nineteen struct {
+	One       tiny
+	Two       tiny
+	Three     tiny
+	Four      tiny
+	Five      tiny
+	Six       tiny
+	Seven     tiny
+	Eight     tiny
+	Nine      tiny
+	Ten       tiny
+	Eleven    tiny
+	Twelve    tiny
+	Thirteen  tiny
+	Fourteen  tiny
+	Fifteen   tiny
+	Sixteen   tiny
+	Seventeen tiny
+	Eighteen  tiny
+	Nineteen  tiny
+}
+
+type Twenty struct {
+	One       tiny
+	Two       tiny
+	Three     tiny
+	Four      tiny
+	Five      tiny
+	Six       tiny
+	Seven     tiny
+	Eight     tiny
+	Nine      tiny
+	Ten       tiny
+	Eleven    tiny
+	Twelve    tiny
+	Thirteen  tiny
+	Fourteen  tiny
+	Fifteen   tiny
+	Sixteen   tiny
+	Seventeen tiny
+	Eighteen  tiny
+	Nineteen  tiny
+	Twenty    tiny
+}
+
+type TwentyOne struct {
+	One       tiny
+	Two       tiny
+	Three     tiny
+	Four      tiny
+	Five      tiny
+	Six       tiny
+	Seven     tiny
+	Eight     tiny
+	Nine      tiny
+	Ten       tiny
+	Eleven    tiny
+	Twelve    tiny
+	Thirteen  tiny
+	Fourteen  tiny
+	Fifteen   tiny
+	Sixteen   tiny
+	Seventeen tiny
+	Eighteen  tiny
+	Nineteen  tiny
+	Twenty    tiny
+	TwentyOne tiny
+}
+
+type TwentyTwo struct {
+	One       tiny
+	Two       tiny
+	Three     tiny
+	Four      tiny
+	Five      tiny
+	Six       tiny
+	Seven     tiny
+	Eight     tiny
+	Nine      tiny
+	Ten       tiny
+	Eleven    tiny
+	Twelve    tiny
+	Thirteen  tiny
+	Fourteen  tiny
+	Fifteen   tiny
+	Sixteen   tiny
+	Seventeen tiny
+	Eighteen  tiny
+	Nineteen  tiny
+	Twenty    tiny
+	TwentyOne tiny
+	TwentyTwo tiny
+}
+
+type TwentyThree struct {
+	One         tiny
+	Two         tiny
+	Three       tiny
+	Four        tiny
+	Five        tiny
+	Six         tiny
+	Seven       tiny
+	Eight       tiny
+	Nine        tiny
+	Ten         tiny
+	Eleven      tiny
+	Twelve      tiny
+	Thirteen    tiny
+	Fourteen    tiny
+	Fifteen     tiny
+	Sixteen     tiny
+	Seventeen   tiny
+	Eighteen    tiny
+	Nineteen    tiny
+	Twenty      tiny
+	TwentyOne   tiny
+	TwentyTwo   tiny
+	TwentyThree tiny
+}
