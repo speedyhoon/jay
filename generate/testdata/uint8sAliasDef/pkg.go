a284292f294e3bb2d234fb73347208b5e20b27a6
Cam <16303791+speedyhoon@users.noreply.github.com>
2024-04-13 09:41:35 +1000

    Add extra tests for []uint8 types.

---
+++ generate/testdata/uint8sAliasDef/pkg.go
@@ -0,0 +1,348 @@
+package main
+
+type uint8s []uint8
+
+type One struct {
+	One uint8s
+}
+
+type Two struct {
+	One uint8s
+	Two uint8s
+}
+
+type Three struct {
+	One   uint8s
+	Two   uint8s
+	Three uint8s
+}
+
+type Four struct {
+	One   uint8s
+	Two   uint8s
+	Three uint8s
+	Four  uint8s
+}
+
+type Five struct {
+	One   uint8s
+	Two   uint8s
+	Three uint8s
+	Four  uint8s
+	Five  uint8s
+}
+
+type Six struct {
+	One   uint8s
+	Two   uint8s
+	Three uint8s
+	Four  uint8s
+	Five  uint8s
+	Six   uint8s
+}
+
+type Seven struct {
+	One   uint8s
+	Two   uint8s
+	Three uint8s
+	Four  uint8s
+	Five  uint8s
+	Six   uint8s
+	Seven uint8s
+}
+
+type Eight struct {
+	One   uint8s
+	Two   uint8s
+	Three uint8s
+	Four  uint8s
+	Five  uint8s
+	Six   uint8s
+	Seven uint8s
+	Eight uint8s
+}
+
+type Nine struct {
+	One   uint8s
+	Two   uint8s
+	Three uint8s
+	Four  uint8s
+	Five  uint8s
+	Six   uint8s
+	Seven uint8s
+	Eight uint8s
+	Nine  uint8s
+}
+
+type Ten struct {
+	One   uint8s
+	Two   uint8s
+	Three uint8s
+	Four  uint8s
+	Five  uint8s
+	Six   uint8s
+	Seven uint8s
+	Eight uint8s
+	Nine  uint8s
+	Ten   uint8s
+}
+
+type Eleven struct {
+	One    uint8s
+	Two    uint8s
+	Three  uint8s
+	Four   uint8s
+	Five   uint8s
+	Six    uint8s
+	Seven  uint8s
+	Eight  uint8s
+	Nine   uint8s
+	Ten    uint8s
+	Eleven uint8s
+}
+
+type Twelve struct {
+	One    uint8s
+	Two    uint8s
+	Three  uint8s
+	Four   uint8s
+	Five   uint8s
+	Six    uint8s
+	Seven  uint8s
+	Eight  uint8s
+	Nine   uint8s
+	Ten    uint8s
+	Eleven uint8s
+	Twelve uint8s
+}
+
+type Thirteen struct {
+	One      uint8s
+	Two      uint8s
+	Three    uint8s
+	Four     uint8s
+	Five     uint8s
+	Six      uint8s
+	Seven    uint8s
+	Eight    uint8s
+	Nine     uint8s
+	Ten      uint8s
+	Eleven   uint8s
+	Twelve   uint8s
+	Thirteen uint8s
+}
+
+type Fourteen struct {
+	One      uint8s
+	Two      uint8s
+	Three    uint8s
+	Four     uint8s
+	Five     uint8s
+	Six      uint8s
+	Seven    uint8s
+	Eight    uint8s
+	Nine     uint8s
+	Ten      uint8s
+	Eleven   uint8s
+	Twelve   uint8s
+	Thirteen uint8s
+	Fourteen uint8s
+}
+
+type Fifteen struct {
+	One      uint8s
+	Two      uint8s
+	Three    uint8s
+	Four     uint8s
+	Five     uint8s
+	Six      uint8s
+	Seven    uint8s
+	Eight    uint8s
+	Nine     uint8s
+	Ten      uint8s
+	Eleven   uint8s
+	Twelve   uint8s
+	Thirteen uint8s
+	Fourteen uint8s
+	Fifteen  uint8s
+}
+
+type Sixteen struct {
+	One      uint8s
+	Two      uint8s
+	Three    uint8s
+	Four     uint8s
+	Five     uint8s
+	Six      uint8s
+	Seven    uint8s
+	Eight    uint8s
+	Nine     uint8s
+	Ten      uint8s
+	Eleven   uint8s
+	Twelve   uint8s
+	Thirteen uint8s
+	Fourteen uint8s
+	Fifteen  uint8s
+	Sixteen  uint8s
+}
+
+type Seventeen struct {
+	One       uint8s
+	Two       uint8s
+	Three     uint8s
+	Four      uint8s
+	Five      uint8s
+	Six       uint8s
+	Seven     uint8s
+	Eight     uint8s
+	Nine      uint8s
+	Ten       uint8s
+	Eleven    uint8s
+	Twelve    uint8s
+	Thirteen  uint8s
+	Fourteen  uint8s
+	Fifteen   uint8s
+	Sixteen   uint8s
+	Seventeen uint8s
+}
+
+type Eighteen struct {
+	One       uint8s
+	Two       uint8s
+	Three     uint8s
+	Four      uint8s
+	Five      uint8s
+	Six       uint8s
+	Seven     uint8s
+	Eight     uint8s
+	Nine      uint8s
+	Ten       uint8s
+	Eleven    uint8s
+	Twelve    uint8s
+	Thirteen  uint8s
+	Fourteen  uint8s
+	Fifteen   uint8s
+	Sixteen   uint8s
+	Seventeen uint8s
+	Eighteen  uint8s
+}
+
+type Nineteen struct {
+	One       uint8s
+	Two       uint8s
+	Three     uint8s
+	Four      uint8s
+	Five      uint8s
+	Six       uint8s
+	Seven     uint8s
+	Eight     uint8s
+	Nine      uint8s
+	Ten       uint8s
+	Eleven    uint8s
+	Twelve    uint8s
+	Thirteen  uint8s
+	Fourteen  uint8s
+	Fifteen   uint8s
+	Sixteen   uint8s
+	Seventeen uint8s
+	Eighteen  uint8s
+	Nineteen  uint8s
+}
+
+type Twenty struct {
+	One       uint8s
+	Two       uint8s
+	Three     uint8s
+	Four      uint8s
+	Five      uint8s
+	Six       uint8s
+	Seven     uint8s
+	Eight     uint8s
+	Nine      uint8s
+	Ten       uint8s
+	Eleven    uint8s
+	Twelve    uint8s
+	Thirteen  uint8s
+	Fourteen  uint8s
+	Fifteen   uint8s
+	Sixteen   uint8s
+	Seventeen uint8s
+	Eighteen  uint8s
+	Nineteen  uint8s
+	Twenty    uint8s
+}
+
+type TwentyOne struct {
+	One       uint8s
+	Two       uint8s
+	Three     uint8s
+	Four      uint8s
+	Five      uint8s
+	Six       uint8s
+	Seven     uint8s
+	Eight     uint8s
+	Nine      uint8s
+	Ten       uint8s
+	Eleven    uint8s
+	Twelve    uint8s
+	Thirteen  uint8s
+	Fourteen  uint8s
+	Fifteen   uint8s
+	Sixteen   uint8s
+	Seventeen uint8s
+	Eighteen  uint8s
+	Nineteen  uint8s
+	Twenty    uint8s
+	TwentyOne uint8s
+}
+
+type TwentyTwo struct {
+	One       uint8s
+	Two       uint8s
+	Three     uint8s
+	Four      uint8s
+	Five      uint8s
+	Six       uint8s
+	Seven     uint8s
+	Eight     uint8s
+	Nine      uint8s
+	Ten       uint8s
+	Eleven    uint8s
+	Twelve    uint8s
+	Thirteen  uint8s
+	Fourteen  uint8s
+	Fifteen   uint8s
+	Sixteen   uint8s
+	Seventeen uint8s
+	Eighteen  uint8s
+	Nineteen  uint8s
+	Twenty    uint8s
+	TwentyOne uint8s
+	TwentyTwo uint8s
+}
+
+type TwentyThree struct {
+	One         uint8s
+	Two         uint8s
+	Three       uint8s
+	Four        uint8s
+	Five        uint8s
+	Six         uint8s
+	Seven       uint8s
+	Eight       uint8s
+	Nine        uint8s
+	Ten         uint8s
+	Eleven      uint8s
+	Twelve      uint8s
+	Thirteen    uint8s
+	Fourteen    uint8s
+	Fifteen     uint8s
+	Sixteen     uint8s
+	Seventeen   uint8s
+	Eighteen    uint8s
+	Nineteen    uint8s
+	Twenty      uint8s
+	TwentyOne   uint8s
+	TwentyTwo   uint8s
+	TwentyThree uint8s
+}
---
+++ generate/testdata/uint8sRequiredAlias/pkg.go
@@ -0,0 +1,348 @@
+package main
+
+type uint8s = []uint8
+
+type One struct {
+	One uint8s `j:",required"`
+}
+
+type Two struct {
+	One uint8s `j:",required"`
+	Two uint8s `j:",required"`
+}
+
+type Three struct {
+	One   uint8s `j:",required"`
+	Two   uint8s `j:",required"`
+	Three uint8s `j:",required"`
+}
+
+type Four struct {
+	One   uint8s `j:",required"`
+	Two   uint8s `j:",required"`
+	Three uint8s `j:",required"`
+	Four  uint8s `j:",required"`
+}
+
+type Five struct {
+	One   uint8s `j:",required"`
+	Two   uint8s `j:",required"`
+	Three uint8s `j:",required"`
+	Four  uint8s `j:",required"`
+	Five  uint8s `j:",required"`
+}
+
+type Six struct {
+	One   uint8s `j:",required"`
+	Two   uint8s `j:",required"`
+	Three uint8s `j:",required"`
+	Four  uint8s `j:",required"`
+	Five  uint8s `j:",required"`
+	Six   uint8s `j:",required"`
+}
+
+type Seven struct {
+	One   uint8s `j:",required"`
+	Two   uint8s `j:",required"`
+	Three uint8s `j:",required"`
+	Four  uint8s `j:",required"`
+	Five  uint8s `j:",required"`
+	Six   uint8s `j:",required"`
+	Seven uint8s `j:",required"`
+}
+
+type Eight struct {
+	One   uint8s `j:",required"`
+	Two   uint8s `j:",required"`
+	Three uint8s `j:",required"`
+	Four  uint8s `j:",required"`
+	Five  uint8s `j:",required"`
+	Six   uint8s `j:",required"`
+	Seven uint8s `j:",required"`
+	Eight uint8s `j:",required"`
+}
+
+type Nine struct {
+	One   uint8s `j:",required"`
+	Two   uint8s `j:",required"`
+	Three uint8s `j:",required"`
+	Four  uint8s `j:",required"`
+	Five  uint8s `j:",required"`
+	Six   uint8s `j:",required"`
+	Seven uint8s `j:",required"`
+	Eight uint8s `j:",required"`
+	Nine  uint8s `j:",required"`
+}
+
+type Ten struct {
+	One   uint8s `j:",required"`
+	Two   uint8s `j:",required"`
+	Three uint8s `j:",required"`
+	Four  uint8s `j:",required"`
+	Five  uint8s `j:",required"`
+	Six   uint8s `j:",required"`
+	Seven uint8s `j:",required"`
+	Eight uint8s `j:",required"`
+	Nine  uint8s `j:",required"`
+	Ten   uint8s `j:",required"`
+}
+
+type Eleven struct {
+	One    uint8s `j:",required"`
+	Two    uint8s `j:",required"`
+	Three  uint8s `j:",required"`
+	Four   uint8s `j:",required"`
+	Five   uint8s `j:",required"`
+	Six    uint8s `j:",required"`
+	Seven  uint8s `j:",required"`
+	Eight  uint8s `j:",required"`
+	Nine   uint8s `j:",required"`
+	Ten    uint8s `j:",required"`
+	Eleven uint8s `j:",required"`
+}
+
+type Twelve struct {
+	One    uint8s `j:",required"`
+	Two    uint8s `j:",required"`
+	Three  uint8s `j:",required"`
+	Four   uint8s `j:",required"`
+	Five   uint8s `j:",required"`
+	Six    uint8s `j:",required"`
+	Seven  uint8s `j:",required"`
+	Eight  uint8s `j:",required"`
+	Nine   uint8s `j:",required"`
+	Ten    uint8s `j:",required"`
+	Eleven uint8s `j:",required"`
+	Twelve uint8s `j:",required"`
+}
+
+type Thirteen struct {
+	One      uint8s `j:",required"`
+	Two      uint8s `j:",required"`
+	Three    uint8s `j:",required"`
+	Four     uint8s `j:",required"`
+	Five     uint8s `j:",required"`
+	Six      uint8s `j:",required"`
+	Seven    uint8s `j:",required"`
+	Eight    uint8s `j:",required"`
+	Nine     uint8s `j:",required"`
+	Ten      uint8s `j:",required"`
+	Eleven   uint8s `j:",required"`
+	Twelve   uint8s `j:",required"`
+	Thirteen uint8s `j:",required"`
+}
+
+type Fourteen struct {
+	One      uint8s `j:",required"`
+	Two      uint8s `j:",required"`
+	Three    uint8s `j:",required"`
+	Four     uint8s `j:",required"`
+	Five     uint8s `j:",required"`
+	Six      uint8s `j:",required"`
+	Seven    uint8s `j:",required"`
+	Eight    uint8s `j:",required"`
+	Nine     uint8s `j:",required"`
+	Ten      uint8s `j:",required"`
+	Eleven   uint8s `j:",required"`
+	Twelve   uint8s `j:",required"`
+	Thirteen uint8s `j:",required"`
+	Fourteen uint8s `j:",required"`
+}
+
+type Fifteen struct {
+	One      uint8s `j:",required"`
+	Two      uint8s `j:",required"`
+	Three    uint8s `j:",required"`
+	Four     uint8s `j:",required"`
+	Five     uint8s `j:",required"`
+	Six      uint8s `j:",required"`
+	Seven    uint8s `j:",required"`
+	Eight    uint8s `j:",required"`
+	Nine     uint8s `j:",required"`
+	Ten      uint8s `j:",required"`
+	Eleven   uint8s `j:",required"`
+	Twelve   uint8s `j:",required"`
+	Thirteen uint8s `j:",required"`
+	Fourteen uint8s `j:",required"`
+	Fifteen  uint8s `j:",required"`
+}
+
+type Sixteen struct {
+	One      uint8s `j:",required"`
+	Two      uint8s `j:",required"`
+	Three    uint8s `j:",required"`
+	Four     uint8s `j:",required"`
+	Five     uint8s `j:",required"`
+	Six      uint8s `j:",required"`
+	Seven    uint8s `j:",required"`
+	Eight    uint8s `j:",required"`
+	Nine     uint8s `j:",required"`
+	Ten      uint8s `j:",required"`
+	Eleven   uint8s `j:",required"`
+	Twelve   uint8s `j:",required"`
+	Thirteen uint8s `j:",required"`
+	Fourteen uint8s `j:",required"`
+	Fifteen  uint8s `j:",required"`
+	Sixteen  uint8s `j:",required"`
+}
+
+type Seventeen struct {
+	One       uint8s `j:",required"`
+	Two       uint8s `j:",required"`
+	Three     uint8s `j:",required"`
+	Four      uint8s `j:",required"`
+	Five      uint8s `j:",required"`
+	Six       uint8s `j:",required"`
+	Seven     uint8s `j:",required"`
+	Eight     uint8s `j:",required"`
+	Nine      uint8s `j:",required"`
+	Ten       uint8s `j:",required"`
+	Eleven    uint8s `j:",required"`
+	Twelve    uint8s `j:",required"`
+	Thirteen  uint8s `j:",required"`
+	Fourteen  uint8s `j:",required"`
+	Fifteen   uint8s `j:",required"`
+	Sixteen   uint8s `j:",required"`
+	Seventeen uint8s `j:",required"`
+}
+
+type Eighteen struct {
+	One       uint8s `j:",required"`
+	Two       uint8s `j:",required"`
+	Three     uint8s `j:",required"`
+	Four      uint8s `j:",required"`
+	Five      uint8s `j:",required"`
+	Six       uint8s `j:",required"`
+	Seven     uint8s `j:",required"`
+	Eight     uint8s `j:",required"`
+	Nine      uint8s `j:",required"`
+	Ten       uint8s `j:",required"`
+	Eleven    uint8s `j:",required"`
+	Twelve    uint8s `j:",required"`
+	Thirteen  uint8s `j:",required"`
+	Fourteen  uint8s `j:",required"`
+	Fifteen   uint8s `j:",required"`
+	Sixteen   uint8s `j:",required"`
+	Seventeen uint8s `j:",required"`
+	Eighteen  uint8s `j:",required"`
+}
+
+type Nineteen struct {
+	One       uint8s `j:",required"`
+	Two       uint8s `j:",required"`
+	Three     uint8s `j:",required"`
+	Four      uint8s `j:",required"`
+	Five      uint8s `j:",required"`
+	Six       uint8s `j:",required"`
+	Seven     uint8s `j:",required"`
+	Eight     uint8s `j:",required"`
+	Nine      uint8s `j:",required"`
+	Ten       uint8s `j:",required"`
+	Eleven    uint8s `j:",required"`
+	Twelve    uint8s `j:",required"`
+	Thirteen  uint8s `j:",required"`
+	Fourteen  uint8s `j:",required"`
+	Fifteen   uint8s `j:",required"`
+	Sixteen   uint8s `j:",required"`
+	Seventeen uint8s `j:",required"`
+	Eighteen  uint8s `j:",required"`
+	Nineteen  uint8s `j:",required"`
+}
+
+type Twenty struct {
+	One       uint8s `j:",required"`
+	Two       uint8s `j:",required"`
+	Three     uint8s `j:",required"`
+	Four      uint8s `j:",required"`
+	Five      uint8s `j:",required"`
+	Six       uint8s `j:",required"`
+	Seven     uint8s `j:",required"`
+	Eight     uint8s `j:",required"`
+	Nine      uint8s `j:",required"`
+	Ten       uint8s `j:",required"`
+	Eleven    uint8s `j:",required"`
+	Twelve    uint8s `j:",required"`
+	Thirteen  uint8s `j:",required"`
+	Fourteen  uint8s `j:",required"`
+	Fifteen   uint8s `j:",required"`
+	Sixteen   uint8s `j:",required"`
+	Seventeen uint8s `j:",required"`
+	Eighteen  uint8s `j:",required"`
+	Nineteen  uint8s `j:",required"`
+	Twenty    uint8s `j:",required"`
+}
+
+type TwentyOne struct {
+	One       uint8s `j:",required"`
+	Two       uint8s `j:",required"`
+	Three     uint8s `j:",required"`
+	Four      uint8s `j:",required"`
+	Five      uint8s `j:",required"`
+	Six       uint8s `j:",required"`
+	Seven     uint8s `j:",required"`
+	Eight     uint8s `j:",required"`
+	Nine      uint8s `j:",required"`
+	Ten       uint8s `j:",required"`
+	Eleven    uint8s `j:",required"`
+	Twelve    uint8s `j:",required"`
+	Thirteen  uint8s `j:",required"`
+	Fourteen  uint8s `j:",required"`
+	Fifteen   uint8s `j:",required"`
+	Sixteen   uint8s `j:",required"`
+	Seventeen uint8s `j:",required"`
+	Eighteen  uint8s `j:",required"`
+	Nineteen  uint8s `j:",required"`
+	Twenty    uint8s `j:",required"`
+	TwentyOne uint8s `j:",required"`
+}
+
+type TwentyTwo struct {
+	One       uint8s `j:",required"`
+	Two       uint8s `j:",required"`
+	Three     uint8s `j:",required"`
+	Four      uint8s `j:",required"`
+	Five      uint8s `j:",required"`
+	Six       uint8s `j:",required"`
+	Seven     uint8s `j:",required"`
+	Eight     uint8s `j:",required"`
+	Nine      uint8s `j:",required"`
+	Ten       uint8s `j:",required"`
+	Eleven    uint8s `j:",required"`
+	Twelve    uint8s `j:",required"`
+	Thirteen  uint8s `j:",required"`
+	Fourteen  uint8s `j:",required"`
+	Fifteen   uint8s `j:",required"`
+	Sixteen   uint8s `j:",required"`
+	Seventeen uint8s `j:",required"`
+	Eighteen  uint8s `j:",required"`
+	Nineteen  uint8s `j:",required"`
+	Twenty    uint8s `j:",required"`
+	TwentyOne uint8s `j:",required"`
+	TwentyTwo uint8s `j:",required"`
+}
+
+type TwentyThree struct {
+	One         uint8s `j:",required"`
+	Two         uint8s `j:",required"`
+	Three       uint8s `j:",required"`
+	Four        uint8s `j:",required"`
+	Five        uint8s `j:",required"`
+	Six         uint8s `j:",required"`
+	Seven       uint8s `j:",required"`
+	Eight       uint8s `j:",required"`
+	Nine        uint8s `j:",required"`
+	Ten         uint8s `j:",required"`
+	Eleven      uint8s `j:",required"`
+	Twelve      uint8s `j:",required"`
+	Thirteen    uint8s `j:",required"`
+	Fourteen    uint8s `j:",required"`
+	Fifteen     uint8s `j:",required"`
+	Sixteen     uint8s `j:",required"`
+	Seventeen   uint8s `j:",required"`
+	Eighteen    uint8s `j:",required"`
+	Nineteen    uint8s `j:",required"`
+	Twenty      uint8s `j:",required"`
+	TwentyOne   uint8s `j:",required"`
+	TwentyTwo   uint8s `j:",required"`
+	TwentyThree uint8s `j:",required"`
+}
