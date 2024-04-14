03cea319cae40d3eb1f07b279aef3fbbee0ae915
Cam <16303791+speedyhoon@users.noreply.github.com>
2024-04-15 00:15:52 +1000

    Add testdata for []int32 & []int64 types.

---
+++ generate/testdata/int32s/pkg.go
@@ -0,0 +1,346 @@
+package main
+
+type One struct {
+	One []int32
+}
+
+type Two struct {
+	One []int32
+	Two []int32
+}
+
+type Three struct {
+	One   []int32
+	Two   []int32
+	Three []int32
+}
+
+type Four struct {
+	One   []int32
+	Two   []int32
+	Three []int32
+	Four  []int32
+}
+
+type Five struct {
+	One   []int32
+	Two   []int32
+	Three []int32
+	Four  []int32
+	Five  []int32
+}
+
+type Six struct {
+	One   []int32
+	Two   []int32
+	Three []int32
+	Four  []int32
+	Five  []int32
+	Six   []int32
+}
+
+type Seven struct {
+	One   []int32
+	Two   []int32
+	Three []int32
+	Four  []int32
+	Five  []int32
+	Six   []int32
+	Seven []int32
+}
+
+type Eight struct {
+	One   []int32
+	Two   []int32
+	Three []int32
+	Four  []int32
+	Five  []int32
+	Six   []int32
+	Seven []int32
+	Eight []int32
+}
+
+type Nine struct {
+	One   []int32
+	Two   []int32
+	Three []int32
+	Four  []int32
+	Five  []int32
+	Six   []int32
+	Seven []int32
+	Eight []int32
+	Nine  []int32
+}
+
+type Ten struct {
+	One   []int32
+	Two   []int32
+	Three []int32
+	Four  []int32
+	Five  []int32
+	Six   []int32
+	Seven []int32
+	Eight []int32
+	Nine  []int32
+	Ten   []int32
+}
+
+type Eleven struct {
+	One    []int32
+	Two    []int32
+	Three  []int32
+	Four   []int32
+	Five   []int32
+	Six    []int32
+	Seven  []int32
+	Eight  []int32
+	Nine   []int32
+	Ten    []int32
+	Eleven []int32
+}
+
+type Twelve struct {
+	One    []int32
+	Two    []int32
+	Three  []int32
+	Four   []int32
+	Five   []int32
+	Six    []int32
+	Seven  []int32
+	Eight  []int32
+	Nine   []int32
+	Ten    []int32
+	Eleven []int32
+	Twelve []int32
+}
+
+type Thirteen struct {
+	One      []int32
+	Two      []int32
+	Three    []int32
+	Four     []int32
+	Five     []int32
+	Six      []int32
+	Seven    []int32
+	Eight    []int32
+	Nine     []int32
+	Ten      []int32
+	Eleven   []int32
+	Twelve   []int32
+	Thirteen []int32
+}
+
+type Fourteen struct {
+	One      []int32
+	Two      []int32
+	Three    []int32
+	Four     []int32
+	Five     []int32
+	Six      []int32
+	Seven    []int32
+	Eight    []int32
+	Nine     []int32
+	Ten      []int32
+	Eleven   []int32
+	Twelve   []int32
+	Thirteen []int32
+	Fourteen []int32
+}
+
+type Fifteen struct {
+	One      []int32
+	Two      []int32
+	Three    []int32
+	Four     []int32
+	Five     []int32
+	Six      []int32
+	Seven    []int32
+	Eight    []int32
+	Nine     []int32
+	Ten      []int32
+	Eleven   []int32
+	Twelve   []int32
+	Thirteen []int32
+	Fourteen []int32
+	Fifteen  []int32
+}
+
+type Sixteen struct {
+	One      []int32
+	Two      []int32
+	Three    []int32
+	Four     []int32
+	Five     []int32
+	Six      []int32
+	Seven    []int32
+	Eight    []int32
+	Nine     []int32
+	Ten      []int32
+	Eleven   []int32
+	Twelve   []int32
+	Thirteen []int32
+	Fourteen []int32
+	Fifteen  []int32
+	Sixteen  []int32
+}
+
+type Seventeen struct {
+	One       []int32
+	Two       []int32
+	Three     []int32
+	Four      []int32
+	Five      []int32
+	Six       []int32
+	Seven     []int32
+	Eight     []int32
+	Nine      []int32
+	Ten       []int32
+	Eleven    []int32
+	Twelve    []int32
+	Thirteen  []int32
+	Fourteen  []int32
+	Fifteen   []int32
+	Sixteen   []int32
+	Seventeen []int32
+}
+
+type Eighteen struct {
+	One       []int32
+	Two       []int32
+	Three     []int32
+	Four      []int32
+	Five      []int32
+	Six       []int32
+	Seven     []int32
+	Eight     []int32
+	Nine      []int32
+	Ten       []int32
+	Eleven    []int32
+	Twelve    []int32
+	Thirteen  []int32
+	Fourteen  []int32
+	Fifteen   []int32
+	Sixteen   []int32
+	Seventeen []int32
+	Eighteen  []int32
+}
+
+type Nineteen struct {
+	One       []int32
+	Two       []int32
+	Three     []int32
+	Four      []int32
+	Five      []int32
+	Six       []int32
+	Seven     []int32
+	Eight     []int32
+	Nine      []int32
+	Ten       []int32
+	Eleven    []int32
+	Twelve    []int32
+	Thirteen  []int32
+	Fourteen  []int32
+	Fifteen   []int32
+	Sixteen   []int32
+	Seventeen []int32
+	Eighteen  []int32
+	Nineteen  []int32
+}
+
+type Twenty struct {
+	One       []int32
+	Two       []int32
+	Three     []int32
+	Four      []int32
+	Five      []int32
+	Six       []int32
+	Seven     []int32
+	Eight     []int32
+	Nine      []int32
+	Ten       []int32
+	Eleven    []int32
+	Twelve    []int32
+	Thirteen  []int32
+	Fourteen  []int32
+	Fifteen   []int32
+	Sixteen   []int32
+	Seventeen []int32
+	Eighteen  []int32
+	Nineteen  []int32
+	Twenty    []int32
+}
+
+type TwentyOne struct {
+	One       []int32
+	Two       []int32
+	Three     []int32
+	Four      []int32
+	Five      []int32
+	Six       []int32
+	Seven     []int32
+	Eight     []int32
+	Nine      []int32
+	Ten       []int32
+	Eleven    []int32
+	Twelve    []int32
+	Thirteen  []int32
+	Fourteen  []int32
+	Fifteen   []int32
+	Sixteen   []int32
+	Seventeen []int32
+	Eighteen  []int32
+	Nineteen  []int32
+	Twenty    []int32
+	TwentyOne []int32
+}
+
+type TwentyTwo struct {
+	One       []int32
+	Two       []int32
+	Three     []int32
+	Four      []int32
+	Five      []int32
+	Six       []int32
+	Seven     []int32
+	Eight     []int32
+	Nine      []int32
+	Ten       []int32
+	Eleven    []int32
+	Twelve    []int32
+	Thirteen  []int32
+	Fourteen  []int32
+	Fifteen   []int32
+	Sixteen   []int32
+	Seventeen []int32
+	Eighteen  []int32
+	Nineteen  []int32
+	Twenty    []int32
+	TwentyOne []int32
+	TwentyTwo []int32
+}
+
+type TwentyThree struct {
+	One         []int32
+	Two         []int32
+	Three       []int32
+	Four        []int32
+	Five        []int32
+	Six         []int32
+	Seven       []int32
+	Eight       []int32
+	Nine        []int32
+	Ten         []int32
+	Eleven      []int32
+	Twelve      []int32
+	Thirteen    []int32
+	Fourteen    []int32
+	Fifteen     []int32
+	Sixteen     []int32
+	Seventeen   []int32
+	Eighteen    []int32
+	Nineteen    []int32
+	Twenty      []int32
+	TwentyOne   []int32
+	TwentyTwo   []int32
+	TwentyThree []int32
+}
---
+++ generate/testdata/int64sAliased/pkg.go
@@ -0,0 +1,348 @@
+package main
+
+type medium = []int64
+
+type One struct {
+	One medium
+}
+
+type Two struct {
+	One medium
+	Two medium
+}
+
+type Three struct {
+	One   medium
+	Two   medium
+	Three medium
+}
+
+type Four struct {
+	One   medium
+	Two   medium
+	Three medium
+	Four  medium
+}
+
+type Five struct {
+	One   medium
+	Two   medium
+	Three medium
+	Four  medium
+	Five  medium
+}
+
+type Six struct {
+	One   medium
+	Two   medium
+	Three medium
+	Four  medium
+	Five  medium
+	Six   medium
+}
+
+type Seven struct {
+	One   medium
+	Two   medium
+	Three medium
+	Four  medium
+	Five  medium
+	Six   medium
+	Seven medium
+}
+
+type Eight struct {
+	One   medium
+	Two   medium
+	Three medium
+	Four  medium
+	Five  medium
+	Six   medium
+	Seven medium
+	Eight medium
+}
+
+type Nine struct {
+	One   medium
+	Two   medium
+	Three medium
+	Four  medium
+	Five  medium
+	Six   medium
+	Seven medium
+	Eight medium
+	Nine  medium
+}
+
+type Ten struct {
+	One   medium
+	Two   medium
+	Three medium
+	Four  medium
+	Five  medium
+	Six   medium
+	Seven medium
+	Eight medium
+	Nine  medium
+	Ten   medium
+}
+
+type Eleven struct {
+	One    medium
+	Two    medium
+	Three  medium
+	Four   medium
+	Five   medium
+	Six    medium
+	Seven  medium
+	Eight  medium
+	Nine   medium
+	Ten    medium
+	Eleven medium
+}
+
+type Twelve struct {
+	One    medium
+	Two    medium
+	Three  medium
+	Four   medium
+	Five   medium
+	Six    medium
+	Seven  medium
+	Eight  medium
+	Nine   medium
+	Ten    medium
+	Eleven medium
+	Twelve medium
+}
+
+type Thirteen struct {
+	One      medium
+	Two      medium
+	Three    medium
+	Four     medium
+	Five     medium
+	Six      medium
+	Seven    medium
+	Eight    medium
+	Nine     medium
+	Ten      medium
+	Eleven   medium
+	Twelve   medium
+	Thirteen medium
+}
+
+type Fourteen struct {
+	One      medium
+	Two      medium
+	Three    medium
+	Four     medium
+	Five     medium
+	Six      medium
+	Seven    medium
+	Eight    medium
+	Nine     medium
+	Ten      medium
+	Eleven   medium
+	Twelve   medium
+	Thirteen medium
+	Fourteen medium
+}
+
+type Fifteen struct {
+	One      medium
+	Two      medium
+	Three    medium
+	Four     medium
+	Five     medium
+	Six      medium
+	Seven    medium
+	Eight    medium
+	Nine     medium
+	Ten      medium
+	Eleven   medium
+	Twelve   medium
+	Thirteen medium
+	Fourteen medium
+	Fifteen  medium
+}
+
+type Sixteen struct {
+	One      medium
+	Two      medium
+	Three    medium
+	Four     medium
+	Five     medium
+	Six      medium
+	Seven    medium
+	Eight    medium
+	Nine     medium
+	Ten      medium
+	Eleven   medium
+	Twelve   medium
+	Thirteen medium
+	Fourteen medium
+	Fifteen  medium
+	Sixteen  medium
+}
+
+type Seventeen struct {
+	One       medium
+	Two       medium
+	Three     medium
+	Four      medium
+	Five      medium
+	Six       medium
+	Seven     medium
+	Eight     medium
+	Nine      medium
+	Ten       medium
+	Eleven    medium
+	Twelve    medium
+	Thirteen  medium
+	Fourteen  medium
+	Fifteen   medium
+	Sixteen   medium
+	Seventeen medium
+}
+
+type Eighteen struct {
+	One       medium
+	Two       medium
+	Three     medium
+	Four      medium
+	Five      medium
+	Six       medium
+	Seven     medium
+	Eight     medium
+	Nine      medium
+	Ten       medium
+	Eleven    medium
+	Twelve    medium
+	Thirteen  medium
+	Fourteen  medium
+	Fifteen   medium
+	Sixteen   medium
+	Seventeen medium
+	Eighteen  medium
+}
+
+type Nineteen struct {
+	One       medium
+	Two       medium
+	Three     medium
+	Four      medium
+	Five      medium
+	Six       medium
+	Seven     medium
+	Eight     medium
+	Nine      medium
+	Ten       medium
+	Eleven    medium
+	Twelve    medium
+	Thirteen  medium
+	Fourteen  medium
+	Fifteen   medium
+	Sixteen   medium
+	Seventeen medium
+	Eighteen  medium
+	Nineteen  medium
+}
+
+type Twenty struct {
+	One       medium
+	Two       medium
+	Three     medium
+	Four      medium
+	Five      medium
+	Six       medium
+	Seven     medium
+	Eight     medium
+	Nine      medium
+	Ten       medium
+	Eleven    medium
+	Twelve    medium
+	Thirteen  medium
+	Fourteen  medium
+	Fifteen   medium
+	Sixteen   medium
+	Seventeen medium
+	Eighteen  medium
+	Nineteen  medium
+	Twenty    medium
+}
+
+type TwentyOne struct {
+	One       medium
+	Two       medium
+	Three     medium
+	Four      medium
+	Five      medium
+	Six       medium
+	Seven     medium
+	Eight     medium
+	Nine      medium
+	Ten       medium
+	Eleven    medium
+	Twelve    medium
+	Thirteen  medium
+	Fourteen  medium
+	Fifteen   medium
+	Sixteen   medium
+	Seventeen medium
+	Eighteen  medium
+	Nineteen  medium
+	Twenty    medium
+	TwentyOne medium
+}
+
+type TwentyTwo struct {
+	One       medium
+	Two       medium
+	Three     medium
+	Four      medium
+	Five      medium
+	Six       medium
+	Seven     medium
+	Eight     medium
+	Nine      medium
+	Ten       medium
+	Eleven    medium
+	Twelve    medium
+	Thirteen  medium
+	Fourteen  medium
+	Fifteen   medium
+	Sixteen   medium
+	Seventeen medium
+	Eighteen  medium
+	Nineteen  medium
+	Twenty    medium
+	TwentyOne medium
+	TwentyTwo medium
+}
+
+type TwentyThree struct {
+	One         medium
+	Two         medium
+	Three       medium
+	Four        medium
+	Five        medium
+	Six         medium
+	Seven       medium
+	Eight       medium
+	Nine        medium
+	Ten         medium
+	Eleven      medium
+	Twelve      medium
+	Thirteen    medium
+	Fourteen    medium
+	Fifteen     medium
+	Sixteen     medium
+	Seventeen   medium
+	Eighteen    medium
+	Nineteen    medium
+	Twenty      medium
+	TwentyOne   medium
+	TwentyTwo   medium
+	TwentyThree medium
+}
